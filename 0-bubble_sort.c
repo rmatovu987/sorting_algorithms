e9718c2058c851f5d74eaf4a3ea8669536aafefa
rmatovu987 <rmatovu987@gmail.com>
2021-10-25 19:47:29 +0300

    ...

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,37 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array in ascending order
- * @array: array of ints to sort
- * @size: size of the array
+ * bubble_sort - sort via bubble method
+ * @array: int
+ * @size: size_t
+ * Return: always 0
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
+	size_t cycle, swap, temp;
+	int pass;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (cycle = 0; cycle < size - 1; cycle++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+
+		for (pass = 0, swap = 0; swap < size - cycle - 1; swap++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[swap + 1] < array[swap])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				pass = 1;
+				temp = array[swap];
+				array[swap] = array[swap + 1];
+				array[swap + 1] = temp;
 				print_array(array, size);
+
 			}
 		}
+
+		if (pass == 0)
+			break;
 	}
 }
